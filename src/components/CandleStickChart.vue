bb5ea929214ca1d3a8dc0acb1a828d549e7e06af
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-12-03 19:12:54 -0300

    feat: create and configure CandleStickChart vue component

---
+++ src/components/CandleStickChart.vue
@@ -0,0 +1,47 @@
+<template>
+  <apexchart type="candlestick" height="350" :options="chartOptions" :series="series">
+  </apexchart>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import VueApexCharts from 'vue3-apexcharts'
+import { Prop } from 'vue-property-decorator'
+
+@Options({
+  components: {
+    apexchart: VueApexCharts
+  }
+})
+export default class CandleStickChart extends Vue {
+  @Prop()
+  candles = []
+
+  chartOptions = {
+    chart: {
+      type: 'candlestick',
+      height: 350
+    },
+    title: {
+      text: 'Bitcoin Last Prices',
+      align: 'center'
+    },
+    xaxis: {
+      type: 'time'
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true
+      }
+    }
+  }
+
+  series = [{
+    data: this.candles
+  }]
+}
+</script>
+
+<style>
+
+</style>
