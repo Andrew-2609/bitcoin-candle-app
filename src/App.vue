b7d7e73123bc987b1ec36ee9779381a0acc25381
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-12-03 19:04:31 -0300

    feat: add and stylize header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
-<template></template>
+<template>
+  <Header></Header>
+</template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import Header from './components/Header.vue'
 
 @Options({
   components: {
-
+    Header
   },
 })
 export default class App extends Vue { }
 </script>
 
 <style>
-
+  body {
+    margin: 0;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="header">
+    <img :src="logo" alt="Bitcoin Logo" />
+    <span>Bitcoin Candle App</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import logo from '../assets/img/logo.png'
+
+export default class Header extends Vue {
+  get logo() {
+    return logo
+  }
+}
+</script>
+
+<style>
+  #header {
+    background-image: linear-gradient(to right, #4d05e8, #4871f7);
+    padding: 30px 10px;
+    margin-bottom: 10px;
+  }
+
+  #header * {
+    vertical-align: middle;
+  }
+
+  #header img {
+    width: 50px;
+  }
+
+  #header span {
+    margin-left: 20px;
+    font-size: 1.5rem;
+    color: white;
+    text-shadow: 2px 2px #333;
+  }
+</style>
